f65e2f4be506da7bf69830cf7ea302e3729e4d84
Duy Cuong <leduycuong726@gmail.com>
2024-11-10 11:19:05 -0800

    build logic sign up

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 // import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -90,6 +91,7 @@ class MyApp extends StatelessWidget {
             Locale('en'),
             Locale('vi'),
           ],
+          builder: EasyLoading.init(),
           debugShowCheckedModeBanner: false,
           home: const SplashView(),
         ),
--- lib/page/sign_up/sign_up_view.dart
+++ lib/page/sign_up/sign_up_view.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
 import 'package:chat_app/bloc/auth/auth_bloc.dart';
 import 'package:chat_app/bloc/auth/auth_event.dart';
 import 'package:chat_app/bloc/auth/auth_state.dart';
+import 'package:chat_app/bloc/user/user_bloc.dart';
+import 'package:chat_app/bloc/user/user_event.dart';
+import 'package:chat_app/bloc/user/user_state.dart';
 import 'package:chat_app/common/values/colors.dart';
 import 'package:chat_app/common/values/icons.dart';
 import 'package:chat_app/common/values/typography.dart';
@@ -8,6 +13,7 @@ import 'package:chat_app/common/widgets/custom_container_sign_in_out.dart';
 import 'package:chat_app/common/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class SignUpView extends StatefulWidget {
@@ -59,175 +65,193 @@ class _SignUpViewState extends State<SignUpView> {
     });
   }
 
+  bool get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      nameError == null &&
+      accept;
+
   @override
   Widget build(BuildContext context) {
     final heightScreen = MediaQuery.of(context).size.height;
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: BlocConsumer<AuthBloc, AuthState>(
-            listener: (context, state) {
-              if (state is AuthFailure) {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  backgroundColor: AppColor.errorColor,
-                  content: Text(state.message),
-                ));
-              } else if (state is AuthAuthenticated) {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  backgroundColor: AppColor.successColor,
-                  content: Text(AppLocalizations.of(context)!.successSignUp),
-                ));
-                Navigator.pop(context);
-              }
-            },
-            builder: (context, state) {
-              if (state is AuthLoading) {
-                return const Center(child: CircularProgressIndicator());
-              }
-              return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: heightScreen * 0.08),
-                      IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        icon: const Icon(
-                          AppIcon.back,
-                          size: 30,
-                          color: AppColor.primaryColor,
+        resizeToAvoidBottomInset: false,
+        body: MultiBlocListener(
+          listeners: [
+            BlocListener<AuthBloc, AuthState>(
+              listener: (context, state) {
+                if (state is AuthFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(AppLocalizations.of(context)!.failedSignUp),
+                      backgroundColor: AppColors.errorColor,
+                    ),
+                  );
+                } else if (state is AuthAuthenticated) {
+                  context.read<UserBloc>().add(UserAddEvent(
+                        email: emailController.text,
+                        name: nameController.text,
+                      ));
+                } else if (state is AuthLoading) {
+                  EasyLoading.show(maskType: EasyLoadingMaskType.black);
+                }
+              },
+            ),
+            BlocListener<UserBloc, UserState>(
+              listener: (context, state) {
+                if (state is UserFailure) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content:
+                          Text(AppLocalizations.of(context)!.failedAddUser),
+                      backgroundColor: AppColors.errorColor,
+                    ),
+                  );
+                } else if (state is UserSuccess) {
+                  log('User added successfully');
+                  Navigator.pop(context);
+                }
+              },
+            ),
+          ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: heightScreen * 0.08),
+                IconButton(
+                  onPressed: () => Navigator.pop(context),
+                  icon: const Icon(
+                    AppIcon.back,
+                    size: 30,
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                SizedBox(height: heightScreen * 0.05),
+                Text(AppLocalizations.of(context)!.register,
+                    style: AppTypography.signInUpTitle),
+                SizedBox(height: heightScreen * 0.07),
+                CustomTextField(
+                  controller: nameController,
+                  labelText: AppLocalizations.of(context)!.fullName,
+                  suffixIcon: AppIcon.person,
+                  onChanged: validateName,
+                  errorText: nameError,
+                ),
+                SizedBox(height: heightScreen * 0.04),
+                CustomTextField(
+                  controller: emailController,
+                  labelText: 'email',
+                  suffixIcon: AppIcon.mail,
+                  onChanged: validateEmail,
+                  errorText: emailError,
+                ),
+                SizedBox(height: heightScreen * 0.04),
+                CustomTextField(
+                  controller: passwordController,
+                  labelText: AppLocalizations.of(context)!.password,
+                  suffixIcon: AppIcon.key,
+                  ispassword: true,
+                  onChanged: validatePassword,
+                  errorText: passwordError,
+                ),
+                SizedBox(height: heightScreen * 0.04),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          accept = !accept;
+                        });
+                      },
+                      child: Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                              color: AppColors.primaryColor, width: 2),
+                          color: accept ? AppColors.primaryColor : Colors.white,
                         ),
+                        child: accept
+                            ? const Icon(
+                                Icons.check,
+                                color: Colors.white,
+                                size: 16,
+                              )
+                            : null,
                       ),
-                      SizedBox(height: heightScreen * 0.05),
-                      Text(AppLocalizations.of(context)!.register,
-                          style: AppTypography.signInUpTitle),
-                      SizedBox(height: heightScreen * 0.07),
-                      CustomTextField(
-                        controller: nameController,
-                        labelText: AppLocalizations.of(context)!.fullName,
-                        suffixIcon: AppIcon.person,
-                        onChanged: validateName,
-                        errorText: nameError,
-                      ),
-                      SizedBox(height: heightScreen * 0.04),
-                      CustomTextField(
-                        controller: emailController,
-                        labelText: 'email',
-                        suffixIcon: AppIcon.mail,
-                        onChanged: validateEmail,
-                        errorText: emailError,
-                      ),
-                      SizedBox(height: heightScreen * 0.04),
-                      CustomTextField(
-                        controller: passwordController,
-                        labelText: AppLocalizations.of(context)!.password,
-                        suffixIcon: AppIcon.key,
-                        ispassword: true,
-                        onChanged: validatePassword,
-                        errorText: passwordError,
-                      ),
-                      SizedBox(height: heightScreen * 0.04),
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                accept = !accept;
-                              });
-                            },
-                            child: Container(
-                              width: 24,
-                              height: 24,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: AppColor.primaryColor, width: 2),
-                                color: accept
-                                    ? AppColor.primaryColor
-                                    : Colors.white,
-                              ),
-                              child: accept
-                                  ? const Icon(
-                                      Icons.check,
-                                      color: Colors.white,
-                                      size: 16,
-                                    )
-                                  : null,
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          Flexible(
-                            child: RichText(
-                                text: TextSpan(
+                    ),
+                    const SizedBox(width: 8),
+                    Flexible(
+                      child: RichText(
+                        text: TextSpan(
+                          text: '${AppLocalizations.of(context)!.iAgreeTo} ',
+                          style: AppTypography.s14w500
+                              .copyWith(color: AppColors.normalColor),
+                          children: [
+                            TextSpan(
                               text:
-                                  '${AppLocalizations.of(context)!.iAgreeTo} ',
-                              style: AppTypography.s14w500
-                                  .copyWith(color: AppColor.normalColor),
-                              children: [
-                                TextSpan(
-                                  text:
-                                      '${AppLocalizations.of(context)!.policies} ',
-                                  style: AppTypography.s14w700,
-                                ),
-                                TextSpan(
-                                  text: '${AppLocalizations.of(context)!.and} ',
-                                  style: AppTypography.s14w500
-                                      .copyWith(color: AppColor.normalColor),
-                                ),
-                                TextSpan(
-                                  text: AppLocalizations.of(context)!.terms,
-                                  style: AppTypography.s14w700,
-                                ),
-                              ],
-                            )),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: heightScreen * 0.05),
-                      CustomContainerSignInOut(
-                        title: AppLocalizations.of(context)!.register,
-                        onTap: () {
-                          if (emailError == null &&
-                              passwordError == null &&
-                              nameError == null &&
-                              accept) {
-                            context.read<AuthBloc>().add(AuthSignUpRequested(
-                                  email: emailController.text,
-                                  password: passwordController.text,
-                                ));
-                          }
-                        },
-                        enable: emailError == null &&
-                            passwordError == null &&
-                            nameError == null &&
-                            accept,
-                      ),
-                      const Spacer(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            '${AppLocalizations.of(context)!.alreadyHaveAnAcount}? ',
-                            style: AppTypography.s14w500
-                                .copyWith(color: AppColor.normalColor),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text(
-                              AppLocalizations.of(context)!.logInNow,
+                                  '${AppLocalizations.of(context)!.policies} ',
                               style: AppTypography.s14w700,
                             ),
-                          ),
-                        ],
+                            TextSpan(
+                              text: '${AppLocalizations.of(context)!.and} ',
+                              style: AppTypography.s14w500
+                                  .copyWith(color: AppColors.normalColor),
+                            ),
+                            TextSpan(
+                              text: AppLocalizations.of(context)!.terms,
+                              style: AppTypography.s14w700,
+                            ),
+                          ],
+                        ),
                       ),
-                      SizedBox(height: heightScreen * 0.04),
-                    ],
-                  ));
-            },
-          )),
+                    ),
+                  ],
+                ),
+                SizedBox(height: heightScreen * 0.05),
+                CustomContainerSignInOut(
+                  title: AppLocalizations.of(context)!.register,
+                  onTap: () {
+                    isFormValid
+                        ? context.read<AuthBloc>().add(AuthSignUpRequested(
+                              email: emailController.text,
+                              password: passwordController.text,
+                            ))
+                        : null;
+                  },
+                  enable: isFormValid,
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '${AppLocalizations.of(context)!.alreadyHaveAnAcount}? ',
+                      style: AppTypography.s14w500
+                          .copyWith(color: AppColors.normalColor),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.logInNow,
+                        style: AppTypography.s14w700,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: heightScreen * 0.04),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
